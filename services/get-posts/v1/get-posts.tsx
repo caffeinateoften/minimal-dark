ff49d053e7294a612ed265d2cb905a6cf389a983
yeeter <yeeter@Not-George-h.local>
2021-04-10 09:09:14 -0400

    feat(ideas): todo

--- services/get-posts/v1/get-posts.tsx
+++ services/get-posts/v1/get-posts.tsx
@@ -3,6 +3,16 @@ import { PostProps } from '../../../lib/components/post/v1/post'
 export function getPosts(): PostProps[] {
     return [
         {
+            id: '3',
+            title: 'Maximizing Freedom for the Uncertain Technocrat',
+            content: 'TODO'
+        },
+        {
+            id: '2',
+            title: 'The Freedom Test',
+            content: 'TODO'
+        },
+        {
             id: '1',
             title: 'Welcome',
             content: 'This is the personal site of the one and only caffeine king. Posts will consist of random trash.'
