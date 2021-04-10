1b05a6bfa6a8c8a6c95f5b384c63cd3fb776ebcc
yeeter <yeeter@Not-George-h.local>
2021-04-10 11:49:53 -0400

    feat(dilligent writer post): init

---
+++ lib/components/page-layout/v2/page-layout.tsx
@@ -0,0 +1,58 @@
+export interface PageLayoutProps {
+    header: React.ReactNode
+    children: React.ReactNode
+}
+
+const MARGIN_TOP = '120px'
+
+const MARGIN_SIDES_EXTRA_SMALL = '5vw';
+const MARGIN_SIDES_SMALL = '10vw';
+const MARGIN_SIDES_MEDIUM = '25vw';
+
+export function PageLayout(props: PageLayoutProps) {
+    return (
+        <div className='page'>
+            <style global jsx>{`
+
+                html, body {
+                    height: 100vh;
+                }
+
+                .page {
+                    position: absolute;
+                    background-color: #F8F8F8;
+                    height: 100vh;
+                }
+
+                .page-content-container {
+                        display: flex;
+                        position: relative;
+                        max-width: 1100px;
+                        margin-top: ${MARGIN_TOP};
+                        margin-left: ${MARGIN_SIDES_EXTRA_SMALL};
+                        margin-right: ${MARGIN_SIDES_EXTRA_SMALL};
+                    }
+                    
+                    @media only screen and (min-width: 600px) {
+                        .page-content-container {
+                            margin-left: ${MARGIN_SIDES_SMALL};
+                            margin-right: ${MARGIN_SIDES_SMALL};
+                        }
+                    }
+
+                    @media only screen and (min-width: 1000px) {
+                        .page-content-container {
+                            margin-left: ${MARGIN_SIDES_MEDIUM};
+                            margin-right: ${MARGIN_SIDES_MEDIUM};
+                        }
+                    }
+            `}</style>
+            <div>
+                {props.header}
+            </div>
+            <div className="page-content-container">
+                {props.children}
+            </div>
+        </div>
+    )
+}
--- services/get-posts/v2/get-posts.tsx
+++ services/get-posts/v2/get-posts.tsx
@@ -76,10 +76,69 @@ export function getPosts(): PostProps[] {
             content: 'TODO'
         },
         {
+            id: '0',
+            title: 'How does a dilligent writer write a sentence?',
+            subtitle: '"...if thought corrupts language, language can also corrupt thought." - George Orwell',
+            content: <div>
+                <h4>
+                    They ask themself:
+            </h4>
+                <ol>
+                    <li>
+                        What am I trying to say?
+                </li>
+                    <li>
+                        What words will express it?
+                </li>
+                    <li>
+                        What image or idiom will make it clearer?
+                </li>
+                    <li>
+                        Is this image fresh enough to have an effect?
+                </li>
+                    <li>
+                        Could I put it more shortly?
+                </li>
+                    <li>
+                        Have I said anything that is avoidably ugly?
+                </li>
+                </ol>
+
+                <h4>
+                    They try to follow these rules:
+            </h4>
+
+                <ul>
+                    <li>
+                        (i) Never use a metaphor, simile, or other figure of speech which you are used to seeing in print.
+                </li>
+                    <li>
+                        (ii) Never use a long word where a short one will do.
+                </li>
+                    <li>
+                        (iii) If it is possible to cut a word out, always cut it out.
+                </li>
+                    <li>
+                        (iv) Never use the passive where you can use the active.
+                </li>
+                    <li>
+                        (v) Never use a foreign phrase, a scientific word, or a jargon word if you can think of an everyday English equivalent.
+                </li>
+                    <li>
+                        (vi) Break any of these rules sooner than say anything outright barbarous.
+                </li>
+                </ul>
+                <br />
+                <div>
+                    I am no dilligent writer, but if I tried to be one, I might see what the outcome of the above process is.
+                </div>
+            </div>
+        },
+        {
             id: '1',
             title: 'Welcome',
             subtitle: 'Hello, Friend.',
             content: 'This is the personal site of the one and only caffeine king. Posts will consist of random trash.'
-        }
+        },
     ]
 }
